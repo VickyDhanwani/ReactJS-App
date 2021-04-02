1950d3960e40052fc8f273e6cf396e7ce296e5a6
VickyDhanwani <vickydhanani11@gmail.com>
2021-04-02 23:45:07 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <ol>
+        <li>Login Component</li>
+        <li>SignUp Component</li>
+        <li>Dashboard</li>
+        <li>Searchbar and Filters</li>
+        <li>organize content</li>
+        <li>CRUPOps</li>
+      </ol>
+    </div>
+  );
+}
+
+export default App;
