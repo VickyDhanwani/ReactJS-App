8872c72b4de1199424018713c929496efeb7078f
VickyDhanwani <vickydhanani11@gmail.com>
2021-04-17 11:43:15 +0530

    addedbackend file

---
+++ Backend.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require( 'body-parser');
+
+var app = express();
+app.use(cors());
+
+    
+var users = [
+    {
+        username : 'abc',
+        password : '#@Dsajf!2'
+    }
+]
+
+app.get('/:username', (req, res) => {
+    
+    res.send(req.params.username)
+});
+
+
+
+var server = app.listen(8081, () => {
+    console.log("Listening...");
+});
